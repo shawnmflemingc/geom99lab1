e0c911abef247c94605246ea3dfa34a5defea97e
shawnmflemingc <shawn.morgan@flemingcollege.ca>
2024-01-19 11:31:29 -0500

    Update index.js

--- placeid/index.js
+++ placeid/index.js
@@ -1,90 +1,64 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// @ts-nocheck TODO remove when fixed
-
-// This sample requires the Places library. Include the libraries=places
+// This sample uses the Place Autocomplete widget to allow the user to search
+// for and select a place. The sample then displays an info window containing
+// the place ID and other information about the place that the user has
+// selected.
+// This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
-// <script
-// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-function initMap(): void {
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      center: { lat: -33.8688, lng: 151.2195 },
-      zoom: 13,
-    }
-  );
-
-  const input = document.getElementById("pac-input") as HTMLInputElement;
-
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+  });
+  const input = document.getElementById("pac-input");
   // Specify just the place data fields that you need.
   const autocomplete = new google.maps.places.Autocomplete(input, {
-    fields: ["place_id", "geometry", "name", "formatted_address"],
+    fields: ["place_id", "geometry", "formatted_address", "name"],
   });
 
   autocomplete.bindTo("bounds", map);
-
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById(
-    "infowindow-content"
-  ) as HTMLElement;
+  const infowindowContent = document.getElementById("infowindow-content");
 
   infowindow.setContent(infowindowContent);
-
-  const geocoder = new google.maps.Geocoder();
 
   const marker = new google.maps.Marker({ map: map });
 
   marker.addListener("click", () => {
     infowindow.open(map, marker);
   });
-
   autocomplete.addListener("place_changed", () => {
     infowindow.close();
 
     const place = autocomplete.getPlace();
 
-    if (!place.place_id) {
+    if (!place.geometry || !place.geometry.location) {
       return;
     }
 
-    geocoder
-      .geocode({ placeId: place.place_id })
-      .then(({ results }) => {
-        map.setZoom(11);
-        map.setCenter(results[0].geometry.location);
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
 
-        // Set the position of the marker using the place ID and location.
-        // @ts-ignore TODO This should be in @typings/googlemaps.
-        marker.setPlace({
-          placeId: place.place_id,
-          location: results[0].geometry.location,
-        });
-
-        marker.setVisible(true);
-
-        infowindowContent.children["place-name"].textContent = place.name;
-        infowindowContent.children["place-id"].textContent = place.place_id;
-        infowindowContent.children["place-address"].textContent =
-          results[0].formatted_address;
-
-        infowindow.open(map, marker);
-      })
-      .catch((e) => window.alert("Geocoder failed due to: " + e));
+    // Set the position of the marker using the place ID and location.
+    // @ts-ignore This should be in @typings/googlemaps.
+    marker.setPlace({
+      placeId: place.place_id,
+      location: place.geometry.location,
+    });
+    marker.setVisible(true);
+    infowindowContent.children.namedItem("place-name").textContent = place.name;
+    infowindowContent.children.namedItem("place-id").textContent =
+      place.place_id;
+    infowindowContent.children.namedItem("place-address").textContent =
+      place.formatted_address;
+    infowindow.open(map, marker);
   });
 }
 
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
 window.initMap = initMap;
-export {};
